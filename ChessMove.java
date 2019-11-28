ad9018f34fce6ed8363e1ea7f0b2a9de527c858f
BananaIguanas <nathanj238@gmail.com>
2019-11-28 16:17:50 +1100

    Added class ChessMove to store move history

--- ChessMove.java
+++ ChessMove.java
@@ -8,15 +8,30 @@ public class ChessMove {
 	public ChessMove(String srcLocationName, String destLocationName) {
 		this.srcLocationName = srcLocationName;
 		this.destLocationName = destLocationName;
-		setSrcLocation();
-		setDestLocation();
+		this.srcLocation = setLocation(srcLocationName);
+		this.destLocation = setLocation(destLocationName);
 	}
 
-	private void setSrcLocation() {
-		
+	private int[] setLocation(String name) {
+		if (name.length() == 2) {
+			char[] tokens = name.toCharArray();
+			int rank = tokens[0] - 'A';
+			int file = tokens[1] - '1';
+			int[] location = new int[]{file, rank};
+			return location;
+		}
+		return null;
 	}
 
-	private void setDestLocation() {
-		
-	}
+	/*
+	 * Main method for debugging.
+	 */
+
+	//public static void main(String[] args)  {
+	//	ChessMove move = new ChessMove("A1", "E3");
+	//	for (int i : move.destLocation) {
+	//		System.out.println(i);
+	//	}
+	//}
+
 } 
